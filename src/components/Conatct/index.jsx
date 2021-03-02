5b39c225655554ca497bc88ba8bb8f803cd24279
Varun Patel <varun@Varuns-MacBook-Pro.local>
2021-03-02 12:15:23 -0500

    Correct Spelling

--- src/components/Conatct/index.jsx
+++ src/components/Conatct/index.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const contact = () => {
     return (
         <div className="contact">
-            <h1>Conatct Me</h1>
+            <h1>Contact Me</h1>
+            <h3>developer.varunpatel@gmail.com</h3>
+            <h3>905 965 1509</h3>
         </div>
     )
 }
